a141a2f46e3caa98ee7fddd91c4ea5c9f84b7fd2
Tonny <antoniopedro@usa.com>
2026-06-01 01:21:48 -0300

    Delete teste.js

--- teste.js
+++
@@ -1,9 +0,0 @@
-console.log("Script externo executando...");
-var div = document.createElement('div');
-div.innerHTML = "<h1>Olá, mundo!</h1>";
-div.style.position = 'absolute';
-div.style.top = '0';
-div.style.left = '0';
-div.style.zIndex = '9999';
-document.body.appendChild(div);
-console.log("Elemento adicionado ao DOM!");
